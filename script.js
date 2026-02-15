7cd494dd6358e05860a8467abe99a50d5e057f5d
tedown2earth <tedmugixd@gmail.com>
2026-02-15 10:28:20 +0300

    Addition of an event listener and preventing the page from realoading

---
+++ script.js
@@ -0,0 +1,7 @@
+   
+
+    const form = document.getElementById('serviceForm');
+    if (form) { 
+        form.addEventListener('submit', function(e)) {
+            e.preventDefault(); 
+        }}
