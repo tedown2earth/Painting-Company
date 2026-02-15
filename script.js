e77efd66f3262ede39dbcf05f011ac7de712cc33
tedown2earth <tedmugixd@gmail.com>
2026-02-15 10:34:19 +0300

    Addition of DOM and a console.log to show the form is submitted successfully

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-   
+document.addEventListener("DOMContentLoaded", function () {
 
-    const form = document.getElementById('serviceForm');
-    if (form) { 
-        form.addEventListener('submit', function(e)) {
-            e.preventDefault(); 
-        }}
+    const form = document.getElementById("serviceForm");
+
+    if (form) {
+        form.addEventListener("submit", function (event) {
+            event.preventDefault(); // Stop page refresh
+
+            console.log("Form submitted successfully");
+        });
+    }
+
+});
